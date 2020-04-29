627a71bb38899847e10dbd933394048d44cd094d
imjeffreylee <imjeffreylee@gmail.com>
2020-04-29 01:04:59 -0400

    Created and migrated user table, created user model and backend auth

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+    validates :email,
+        :password_digest,
+        :session_token,
+        :first_name,
+        :last_name,
+        :status,
+        :industry,
+        :role,
+        presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+    validates :email, :session_token, uniqueness: true
+
+    attr_reader password
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        user && user.is_password?(password) ? user : nil
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+end
