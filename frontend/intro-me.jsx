4324aca884ffb313fd9378ac82b329c6b83da1a1
imjeffreylee <imjeffreylee@gmail.com>
2020-05-01 20:16:47 -0400

    Set up state shape in index.jsx

--- frontend/intro-me.jsx
+++ frontend/intro-me.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import configureStore from './store/store';
+
 document.addEventListener("DOMContentLoaded", () => {
+    let store;
+    let preLoadedState;
+    if (window.currentUser) {
+        preLoadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        }
+        store = configureStore(preLoadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to Intro-Me</h1>, root);
 });
