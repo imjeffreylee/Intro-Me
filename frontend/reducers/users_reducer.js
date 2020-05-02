d8860ec337d83f02798a97e2261eebbedcbefcc7
imjeffreylee <imjeffreylee@gmail.com>
2020-05-01 20:13:23 -0400

    Created users reducer

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,30 @@
+import {
+    RECEIVE_CURRENT_USER,
+    RECEIVE_USER,
+    RECEIVE_USERS,
+} from "../actions/session_actions";
+
+import merge from "lodash/merge";
+
+const usersReducer = (state = {}, action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return merge({}, state, {
+                [action.user.id]: action.user,
+            });
+
+        case RECEIVE_USER:
+            return merge({}, state, {
+                [action.user.id]: action.user,
+            });
+
+        case RECEIVE_USERS:
+            return action.users;
+
+        default:
+            return state;
+    }
+};
+
+export default usersReducer;
